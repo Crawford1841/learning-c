deb67a2675ea807e49c26c954f084b9c772afb65
huangwei <huangwei0246@gmail.com>
2024-03-21 22:30:07 +0800

    第二课时的作业完成60%

--- day_02/MyLine.cpp
+++ day_02/MyLine.cpp
@@ -1,59 +1,62 @@
+/*
+*	04737 c++ 自学考试2019版 第二章课后练习
+*	程序设计题 3
+*	需求:设计并实现一个类MyLine 
+*/
+//标准流 
 #include<iostream>
+//科学计算函数 
 #include<cmath>
 using namespace std;
 
-class Line{
-    private:
-        double x1;
-        double y1;
-    public:
-        Line(double x1,double y1){
-            this->x1 = x1;
-            this->y1 = y1;
-        }
-    /*
-        直线斜率定义：一由一条直线与右边X轴所成的角的正切。
-        ########直线与X轴正向的夹角叫倾斜角 范围是[0,180°) 左闭右开#######
-        公    式 k=(y2-y1)/(x2-x1)
-    */
-        void slope(double x2,double y2){
-             double k = (y2-this->y1)/(x2 - this->x1);
-             if(k==x1){
-                cout<<"该直线没有斜率"<<endl;
-             }else{
-                cout<<"该直线的斜率是："<<k<<endl;
-             }
-        }
-    /*通过判断斜率是否相等 判断是否在直线
-    * ( endY - beginY ) / ( endX - beginX ) ==( Y - beginY ) / ( X - beginX )
-    * 通过直线斜率 代入直线方程判断
-    * 直线方程： y-beginY =k * (x - beginX)
-    * */
-        void pointOnSolpe(double x2,double y2){
-            double k  = (y2-this->y1)/ (x2 - this->x1);
-            if(y2 - this->y1==k * (x2 - this->x1)){
-                cout<<"该坐标在直线上！"<<endl;
-            }else{
-                cout<<"该坐标不在直线上！"<<endl;
-            } 
-        }
-        /**
-         * 求点到直线Ax + By + C = 0的距离公式为：
-            d = |AX0 + BY0 + C| /[(A^2 + B^2)的算术平方根]
-            abs:绝对值
-            sqrt:平方根
-            pow:幂
-        */
-       double distanceToPoint(double x2,double y2){
-            double slope = (y2 - this->y1) / (x2 - this->x1);
-            return abs(slope * x2 - y2 + this->y1) / sqrt(pow(slope,2)+1);
-       }
-
+class MyLine
+{
+	private:
+		//点1 
+		double p1_x,p1_y;
+		//点2 
+		double p2_x,p2_y;
+	public:
+		//construtor
+		MyLine(double p1_x,double p1_y,double p2_x,double p2_y); 
+		//直线的斜率 
+		double slope();
+		//点是否在线上
+		bool isOnLine(double x,double y);
+		//点到直线的距离
+		double distance(double x,double y); 
 };
+MyLine::MyLine(double x1,double y1,double x2,double y2)
+{
+	p1_x = x1;
+	p1_y = y1;
+	p2_x = x2;
+	p2_y = y2;
+}
+//斜率 公式：k=(y2-y1)/(x2-x1) 
+double MyLine::slope()
+{
+	return (p2_y - p1_y)/(p2_x - p1_x);
+}
+//点是否在线上  y-beginY =k * (x - beginX)
+bool MyLine::isOnLine(double x,double y)
+{
+    bool isOn = (y-p1_y) ==(slope()*(x-p1_x));
+	return isOn;
+}
+//点到直线的距离 d = [AX0 + BY0 + C的绝对值]/[(A^2 + B^2)的算术平方根]
+double MyLine::distance(double x,double y)
+{
+	double a = p2_y-p1_y;
+	double b = p1_x-p2_x;
+	double c = (p2_x*p1_y) - (p1_x*p2_y);
+	return abs(a*x+b*y+c) / sqrt(a*a+b*b);
+}
 
-
-int main(){ 
-
-
-    return 0;
+int main()
+{
+	MyLine test(3,3,8,8);
+	cout<<test.slope()<<endl;
+	cout<<test.isOnLine(6,6)<<endl;
+	cout<<test.distance(6,6)<<endl;
 }
